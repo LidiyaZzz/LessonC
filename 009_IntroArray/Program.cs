5025aead0bc0449aab1196ae2d1a8b2fa5a58ecd
LidiyaZzz <Lida.ngs@mail.ru>
2023-01-17 23:30:21 +0700

    009-IntroArray

---
+++ 009_IntroArray/Program.cs
@@ -0,0 +1,43 @@
+﻿void FillArray(int[] collection)
+{
+  for (int i = 0; i < collection.Length; i++)
+  {
+    collection[i] = new Random().Next(1, 10);
+  }
+}
+
+void PrintArray(int[] collection)
+{
+  for (int i = 0; i < collection.Length; i++)
+  {
+    Console.WriteLine(collection[i]);
+  }
+}
+
+int IndexOf(int[] collection, int find)
+{
+  int position = -1;
+  for (int i = 0; i < collection.Length; i++)
+  {
+    if (collection[i] == find)
+    {
+      position = i;
+      break;
+    }
+  }
+  return position;
+}
+
+int[] nembers = new int[10];
+
+
+FillArray(nembers);
+PrintArray(nembers);
+Console.WriteLine(" ");
+
+int position = IndexOf(nembers, 4);
+Console.WriteLine(position);
+
+
+
+
